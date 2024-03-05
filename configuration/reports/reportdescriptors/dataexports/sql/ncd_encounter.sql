efeda44b7df09f1ef514cf02eb9a9356a4df51ad
dmdesimone <ddesimone@pih.org>
2024-03-05 12:30:14 -0500

    SL-532 add hospitalization_12_months

--- configuration/reports/reportdescriptors/dataexports/sql/ncd_encounter.sql
+++ configuration/reports/reportdescriptors/dataexports/sql/ncd_encounter.sql
@@ -36,7 +36,7 @@ create temporary table temp_ncd
  bmi                                 varchar(255),     
  obesity                             bit,              
  number_days_hospitalized            double,           
--- hospitalizations_last_12_months     double,        
+ hospitalizations_last_12_months     double,        
  last_hospitalization_discharge_date datetime,      
  last_hospitalization_outcome        varchar(255),  
  number_hospitalizations_ncd                double,        
@@ -287,8 +287,8 @@ set obesity =
 update temp_ncd t
 set number_days_hospitalized = obs_value_numeric_from_temp(encounter_id, 'PIH','2872');
 
--- update temp_ncd t
--- set hospitalizations_last_12_months = obs_value_numeric_from_temp(encounter_id, 'PIH','5704');
+update temp_ncd t
+ set hospitalizations_last_12_months = obs_value_numeric_from_temp(encounter_id, 'CIEL','5704');
 
 update temp_ncd t
 set last_hospitalization_discharge_date = obs_value_datetime_from_temp(encounter_id, 'PIH','3800');
@@ -680,7 +680,7 @@ fbg_level,
 rbg_level,
 bmi,
 obesity,
--- hospitalizations_last_12_months,
+hospitalizations_last_12_months,
 last_hospitalization_discharge_date,
 last_hospitalization_outcome,
 number_hospitalizations_ncd,
